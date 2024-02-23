18cc299ccc6a17827041bbe7f8ca6015ad0357db
Stu <stumac1000@yahoo.com>
2024-02-23 14:30:13 -0800

    Fix breed name issue The breed name was hovering too high over the breed image. I used grid's "align:end" property to fix this.

--- src/index.js
+++ src/index.js
@@ -18,4 +18,4 @@ displayFavicon();
 //! **`` npx webpack --watch FOOOOOOOL
 //todo **`` Add a loading screen
 //todo **`` Get rid of helper console messages
-//todo **`` The '.list-wrapper' subgrid and grid row behavior is acting funny. When you search 'poodle' or 'belgian' and only two dogs show up, the names are far from the pictures. Mess with the 'auto row'
+//todo **`` Update README.md
